cb83a1fbf0f01afc3331db7e17f4319979e59b5e
marknesh <markmunene.nesh@gmail.com>
2020-01-23 15:32:10 +0300

    push to master

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient } from '@angular/common/http';
 import {Http} from '@angular/http'
-import 'rxjs/add/operator/map'
+// import 'rxjs/add/operator/map'
 import { environment } from 'src/environments/environment';
 import { from } from 'rxjs';
 
@@ -17,7 +17,7 @@ repos:any
 
 
   constructor(private http:HttpClient) {
-    this.username="marknesh"
+   
   }
 
 
